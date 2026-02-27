a1d3fc07a86c968e9f9a0f15c27d650b53eb257c
Reyev123 <alexander.rayev@infineon.com>
2026-02-27 12:06:41 -0500

    Update master.sh
    title_prefix added if config.json exists for a submodule

--- .github/scripts/master.sh
+++ .github/scripts/master.sh
@@ -7,7 +7,7 @@ python3 -c "
 import json
 import os
 
-TITLE_PREFIX = 'DEEPCRAFT\u2122 Studio '
+TITLE_PREFIX = 'DEEPCRAFT\u2122 '
 GITHUB_BASE_URL = os.environ.get('GITHUB_BASE_URL', 'https://github.com').rstrip('/')
 
 
@@ -15,23 +15,49 @@ def has_excluded_prefix(path_parts):
     return any(part.startswith('_') or part.startswith('*') for part in path_parts)
 
 
-def normalize_metadata(obj, repo_url):
+def get_module_title_prefix(path_parts, cache):
+    module_root = path_parts[0] if path_parts else ''
+    if module_root in cache:
+        return cache[module_root]
+
+    module_title_prefix = ''
+    config_path = os.path.join('.', module_root, 'config.json')
+    if module_root and os.path.isfile(config_path):
+        try:
+            with open(config_path, 'r') as config_file:
+                config_data = json.load(config_file)
+            config_prefix = config_data.get('title_prefix')
+            if isinstance(config_prefix, str):
+                module_title_prefix = config_prefix
+            elif config_prefix is not None:
+                print(f'Warning: title_prefix in {config_path} is not a string, ignoring it')
+        except Exception as e:
+            print(f'Warning: Error reading {config_path}: {e}')
+
+    cache[module_root] = module_title_prefix
+    return module_title_prefix
+
+
+def normalize_metadata(obj, repo_url, base_title_prefix, module_title_prefix):
     if isinstance(obj, dict):
         if obj.get('label') == 'GitHub':
             obj['url'] = repo_url
 
         for k, v in list(obj.items()):
             if k == 'title' and isinstance(v, str):
-                if not v.startswith(TITLE_PREFIX):
-                    obj[k] = f'{TITLE_PREFIX}{v}'
+                title_value = v[len(base_title_prefix):] if v.startswith(base_title_prefix) else v
+                if module_title_prefix and not title_value.startswith(module_title_prefix):
+                    title_value = f'{module_title_prefix}{title_value}'
+                obj[k] = f'{base_title_prefix}{title_value}'
             else:
-                normalize_metadata(v, repo_url)
+                normalize_metadata(v, repo_url, base_title_prefix, module_title_prefix)
     elif isinstance(obj, list):
         for item in obj:
-            normalize_metadata(item, repo_url)
+            normalize_metadata(item, repo_url, base_title_prefix, module_title_prefix)
 
 
 cards = {}
+module_title_prefix_cache = {}
 # Search for metadata.json files in subdirectories (pattern */*/metadata.json)
 for root, dirs, files in os.walk('.'):
     # Prune excluded directories while walking
@@ -49,6 +75,7 @@ for root, dirs, files in os.walk('.'):
                     metadata = json.load(f)
 
                 repo_url = f'{GITHUB_BASE_URL}/{rel_path.replace(os.sep, '/')}'
+                module_title_prefix = get_module_title_prefix(path_parts, module_title_prefix_cache)
                 
                 # Use the leaf directory name as the key
                 key = os.path.basename(root)
@@ -57,12 +84,12 @@ for root, dirs, files in os.walk('.'):
                 if isinstance(metadata, list):
                     # If it's an array, create separate entries for each item
                     for i, item in enumerate(metadata):
-                        normalize_metadata(item, repo_url)
+                        normalize_metadata(item, repo_url, TITLE_PREFIX, module_title_prefix)
                         entry_key = f'{key}_{i+1}' if len(metadata) > 1 else key
                         cards[entry_key] = item
                 else:
                     # If it's a single object, use directory name as key
-                    normalize_metadata(metadata, repo_url)
+                    normalize_metadata(metadata, repo_url, TITLE_PREFIX, module_title_prefix)
                     cards[key] = metadata
                     
             except Exception as e:
